ed58e035658adbc94c9d7284ec98eabcccbd4c63
Iacopo Moles <iacopo@icpmol.es>
2025-03-30 11:24:18 +0200

    end of deadzone identification
    Signed-off-by: Iacopo Moles <iacopo@icpmol.es>

--- estimated_parameters_description.m
+++ estimated_parameters_description.m
@@ -2,11 +2,11 @@
 
 % Motor 0
 
-dc_gain0 = -1.58; % v/(rad/s)
-tau0 = 0.0505;   % 1/s
+dc_gain0 = -1.4479; % v/(rad/s)
+tau0 = 0.0401;   % 1/s
 
 % Motor 1
     
-dc_gain1 = -1.65; % v/(rad/s)
-tau1 = 0.0554;   % 1/s
+dc_gain1 = -1.4684; % v/(rad/s)
+tau1 = 0.0409;   % 1/s
 
